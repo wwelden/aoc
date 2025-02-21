48f7d0cb7c904b73da26a7204e6a89de12ae5b61
wwelden <wwelden@trinity.edu>
2025-02-21 11:01:42 -0600

    parsing

--- 2024/day08/D8A.go
+++ 2024/day08/D8A.go
@@ -1,5 +1,10 @@
 package day08
 
+import (
+	"aoc/2024/util"
+	"strings"
+)
+
 const testInput = `......#....#
 ...#....0...
 ....#0....#.
@@ -13,11 +18,21 @@ const testInput = `......#....#
 ..........#.
 ..........#.`
 
+func parse(input string) []string {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	// var results [][]string
+	// for line := range lines {
+	// }
+	return lines
+}
+
 func testDay8PartA() {
 
 }
 
 func SolveDay8PartA() {
 	testDay8PartA()
+	file := util.ReadFile("/Users/williamwelden/Developer/aoc/2024/day08/Input.txt")
+	data := parse(file)
 
 }
